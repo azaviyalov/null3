25a3d5cf7e87fe1ca95305b1aac2c39c04d119bf
Anton Zaviyalov <antonzaviyalov@gmail.com>
2025-07-28 14:02:13 +0300

    feat: update CORS configuration to conditionally enable based on environment variable

--- backend/internal/core/server/echo.go
+++ backend/internal/core/server/echo.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"os"
+
 	"github.com/azaviyalov/null3/backend/internal/core/logging"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -10,16 +12,11 @@ func NewEchoServer() *echo.Echo {
 	e := echo.New()
 	e.HideBanner = true
 	e.HidePort = true
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:4200"},
-		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
-		AllowHeaders: []string{
-			echo.HeaderOrigin,
-			echo.HeaderContentType,
-			echo.HeaderAccept,
-			echo.HeaderAuthorization,
-		},
-	}))
+
+	// Enable cors only if the frontend is not served from the same server.
+	if os.Getenv("ENABLE_FRONTEND_DIST") != "true" {
+		e.Use(middleware.CORS())
+	}
 	e.Use(logging.RequestLogger())
 	e.Use(middleware.Recover())
 
